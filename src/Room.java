07bcc04cb9730247a8261c192c3047f48580a25f
Garrett Lawless <garrett@blahs-MacBook-Pro.local>
2019-12-04 15:58:00 -0800

    Created Maze and Room Objects

---
+++ src/Room.java
@@ -0,0 +1,17 @@
+
+public class Room 
+{
+
+//integers to keep track of n, s, e, w
+	// hashmap to keep track of objects?
+	
+	int northWall, eastWall, southWall, westWall;
+	
+	
+	
+	@Override
+	public String toString() 
+	{
+		return null;
+	}
+}
