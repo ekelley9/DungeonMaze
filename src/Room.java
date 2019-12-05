55e39720ee8688e08aa786745dded6ab33c8a886
ekelley9 <57920465+ekelley9@users.noreply.github.com>
2019-12-05 09:01:37 -0800

    added RoomPool class and added a hashmap of objects to the rooms

--- src/Room.java
+++ src/Room.java
@@ -1,11 +1,23 @@
-
+import java.util.*;
 public class Room 
 {
 
 //integers to keep track of n, s, e, w
-	// hashmap to keep track of objects?
-	
 	int northWall, eastWall, southWall, westWall;
+	
+// hashmap to keep track of Objects in the room
+	private HashMap<String, Object> roomContents = new HashMap<>();
+	
+	public Room(int northWall, int eastWall, int southWall, int westWall) {
+		this.northWall = northWall;
+		this.eastWall = eastWall;
+		this.southWall = southWall;
+		this.westWall = westWall;
+	}
+	
+	public HashMap getContents() {
+		return roomContents;
+	}
 	
 	
 	
