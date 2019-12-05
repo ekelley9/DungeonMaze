b4c9cdc16409d77be555c17f13378daa7228dee0
ekelley9 <57920465+ekelley9@users.noreply.github.com>
2019-12-05 09:29:40 -0800

    Added the DonutSinger and implemented the heal ability in LawMan

--- src/LawMan.java
+++ src/LawMan.java
@@ -1,15 +1,21 @@
-
+import java.util.*;
 public class LawMan extends HeistCharacter{
 
 	private int numTurns;
+	private int chanceToHeal;
+	private int maxHeal;
+	private int minHeal;
 	private Abilities specialMove;
 	
 	
-	public LawMan (String name, int hitPoints, int attackSpeed, 
-	double chanceToHit, int damageMin, int damageMax, 
-	double chanceToDodge, Abilities specialMove) {
+	public LawMan(String name, int hitPoints, int attackSpeed, double chanceToHit, int chanceToHeal, int damageMin,
+			int damageMax, int minHeal, int maxHeal, Abilities specialMove) {
 		super(name, hitPoints, attackSpeed, chanceToHit, damageMin, damageMax, new BasicAttack());
-		this.specialMove = specialMove;
+		
+		this.chanceToHeal = chanceToHeal;
+		this.maxHeal = maxHeal;
+		this.minHeal = minHeal;
+
 	}
 
 	public int getNumTurns() {
@@ -28,5 +34,21 @@ public class LawMan extends HeistCharacter{
 		super.subtractHitPoints(damage);
 	}
 
+	public void heal() {
+		boolean canHeal;
+		int healPoints;
+		Random rand = new Random(System.currentTimeMillis());
+
+		canHeal = (rand.nextInt(100)+1 <= chanceToHeal) && (getHitPoints() > 0);
+
+		if (canHeal) {
+			healPoints = (int) (rand.nextInt(100)+1 * (maxHeal - minHeal + 1)) + minHeal;
+			addHitPoints(healPoints);
+			System.out.println(getName() + " healed himself for " + healPoints + " points.\n"
+					+ "Total hit points remaining are: " + getHitPoints());
+			System.out.println();
+		}
+
+	}
 }
 
