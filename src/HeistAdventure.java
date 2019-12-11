5afa0b0e702ace8f1dab6257c41d26916d2b35f0
James Schofield <jschofield1@eagles.ewu.edu>
2019-12-11 11:57:17 -0800

    added caretaker stuff to HeistAdventure

--- src/HeistAdventure.java
+++ src/HeistAdventure.java
@@ -1,6 +1,13 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.*;
+
 public class HeistAdventure {
-	private Scanner kb = new Scanner(System.in);
+	private static Scanner kb = new Scanner(System.in);
 	
 	public static void main(String[] args) {
 		Museum java;
@@ -42,11 +49,72 @@ public class HeistAdventure {
 				return false;
 			default:
 				System.out.println("Please enter a valid selection");
-			
 			}
-			
 		}
 	}
 	
+	private static boolean decision(String str) {
+		while (true) {
+			System.out.print(str);
+			String selection = kb.nextLine().toUpperCase();
+			switch (selection) {
+			case "Y":
+				return true;
+			case "N":
+				return false;
+			default:
+				System.out.print("Make a valid selection: ");
+			}
+		}
+	}
 	
+	private static boolean isSaveGame() {
+		File file = new File("savedMuseum.txt");
+		if (file.exists()) {
+			return true;
+		}
+		file.delete();
+		return false;
+	}
+	
+	public static void saveGame(Museum theMuseum, Robber theRobber) {
+		if (isSaveGame() && !decision("Save over your previous save? Y/N: ")) {
+			return;
+		}
+		try {
+			ObjectOutputStream museumToSave = new ObjectOutputStream(new FileOutputStream("savedMuseum.txt"));
+			ObjectOutputStream robberToSave = new ObjectOutputStream(new FileOutputStream("savedRobber.txt"));
+			museumToSave.writeObject(theMuseum);
+			robberToSave.writeObject(theRobber);
+			museumToSave.close();
+			robberToSave.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		System.out.println("Game saved.");
+	}
+	
+	public static Museum loadMuseum() {
+		try {
+			ObjectInputStream museumToLoad = new ObjectInputStream(new FileInputStream("savedMuseum.txt"));
+			Museum museum = (Museum) museumToLoad.readObject();
+			museumToLoad.close();
+			return museum;
+		} catch (ClassNotFoundException | IOException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	public static Robber loadRobber() {
+		try {
+			ObjectInputStream robberToLoad = new ObjectInputStream(new FileInputStream("savedRobber.txt"));
+			Robber robber = (Robber) robberToLoad.readObject();
+			robberToLoad.close();
+			return robber;
+		} catch (ClassNotFoundException | IOException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
 }
